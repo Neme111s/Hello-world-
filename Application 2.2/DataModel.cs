da36e93e5fb2206fcee37a77d221b14588905092
Neme111s <https://github.com/Neme111s/Hello-world-.git>
2022-04-07 00:34:27 +0300

    Laba №2

---
+++ Application 2.2/DataModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application_2._2
+{
+    public class DataModel
+    {
+        public System.DateTime DateTime { get; set; }
+
+        public double Value { get; set; }
+    }
+}
